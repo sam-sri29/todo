399ccd82b181c231d69a5ecc9ed99a979cb8e79c
sameer srivastava <sameersrivastava7646@gmail.com>
2026-02-22 14:54:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { MdDeleteOutline } from "react-icons/md";
+
+
+const App = () => {
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTask = () => {
+    if (input.trim() === "") return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: input,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setInput("");
+  }
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+
+
+  return (
+    <>
+      <div className='min-h-screen flex items-center justify-start pt-15 flex-col '>
+
+        <div className="bg-white border rounded-lg shadow-lg flex flex-col items-center p-6 overflow-hidden">
+          {/* TODO TEXT */}
+
+          <div className='mt-10 text-2xl'>
+            <h1>TODO LIST </h1>
+          </div>
+
+          {/* INPUT
+
+        */}
+          <div className=''>
+            <input type="text" value={input} onChange={(e) => { setInput(e.target.value) }} className='bg-red-50 border-solid rounded-lg h-10 w-100 m-5 pl-3' placeholder='Enter task' />
+            <button className='bg-red-200 border-solid rounded-lg p-2.5 text-md pl-2' onClick={addTask}>Add</button>
+          </div>
+
+          {/* CONTENT */}
+
+
+          <div>
+            <ul >
+              {todos.map((todo) => (
+                <li key={todo.id} className='flex justify-between gap-20 mb-5 items-center'>
+                  <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} className='size-4' />
+                  <span className={`ml-2 ${todo.completed ? "line-through" : "no-underline"} text-xl flex items-center`}>
+                    {todo.text}
+                  </span>
+                  <button onClick={() => deleteTodo(todo.id)} className='size-4'><MdDeleteOutline className='size-7' /></button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
